c68abf20a2df05f9fdc9fd1ce3f4def233fa7c9e
Wessel Valkenburg <mail@wesselvalkenburg.nl>
2019-01-10 09:52:57 +0100

    First commit: plugin ready.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Leaflet.Control.DetailLevel
 Add a simple control to bump the zoomOffset beyond the default / retine / hdpi setting. This allows you to view higher zoom-level tiles in a large display. Mind your bandwidth, though.
+
+# Demo
+[Demo](https://htmlpreview.github.io/?https://github.com/valkenburg/Leaflet.Control.DetailLevel/blob/master/demo.html)
+
+# Usage
+The simplest is to include this plugin after including leaflet:
+
+```html
+    <script src="leaflet.js"></script>
+    <script src="leaflet.control.detaillevel.js"></script>
+```
+
+and then add your control using
+```javascript
+    let detailControl = new L.Control.DetailLevel(0, 4);
+    detailControl.addTo(map);
+```
+
+where the  arguments to the constructor are your mininum and maximum detail level == minimum/maximum zoomOffset. Please be aware that the number of tiles on display grows as `2^(zoomOffset)`, meaning that with `zoomOffset = 3` you already display `8` times as many tiles as you would ordinarily.
+
+## License
+
+This plugin is licensed under the MIT license, which is included in the head of the source file. Meaning: do whatever you want, but you cannot blame me if anything breaks.
+
---
+++ leaflet.control.detaillevel.js
@@ -0,0 +1,145 @@
+/**
+    L.Control.DetailLevel by Wessel Valkenburg, released under MIT License.
+
+    Copyright 2019 Wessel Valkenburg
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+L.Control.DetailLevel = L.Control.extend({
+
+    options: {
+        position: "topright"
+    },
+
+    initialize : function (minOffset, maxOffset) {
+        /* leave the possibility open for a user to have layers with different
+           zoomOffsets already. This control should be agnostic about that, so
+           we track our internal counter which satisfies the set maximum, and just
+           add this number to whatever the initialized layers have already set. */
+        this.internalOffsetCounter = 0;
+
+        /* default to a maximum offset of 3, since 2^3 = 8 tiles per canonical tile already. */
+        this.maxOffset = maxOffset == null ? 3 : Math.max(maxOffset, 0);
+        this.minOffset = minOffset == null ? 0 : Math.max(minOffset, -1);
+
+        /* avoid too many taps on the buttons when the map is still updating. */
+        this.updateLock = false;
+    },
+
+    onMapStartLoad : function () {
+        /* block changes during map building */
+        this.updateLock = true;
+    },
+
+    onMapFinishLoad : function () {
+        /* unblock changes after map finished building */
+        this.updateLock = false;
+    },
+
+    attachLoadListeners : function (map) {
+        map.on("loading", this.onMapStartLoad.bind(this));
+        map.on("load", this.onMapFinishLoad.bind(this));
+    },
+
+    onAdd : function (map) {
+
+        this.map = map;
+
+        this.attachLoadListeners(map);
+
+        this.container = L.DomUtil.create("DIV", "leaflet-bar leaflet-control leaflet-control-custom");
+
+        this.aPlus = this.makeButton("+", this.increaseDetail.bind(this));
+
+        this.aMinus = this.makeButton("-", this.decreaseDetail.bind(this));
+
+        this.container.appendChild(this.aPlus);
+        this.container.appendChild(this.aMinus);
+
+        return this.container;
+    },
+
+    makeButton : function (innerText, callback) {
+        let result = L.DomUtil.create("A");
+        result.href = "#";
+        result.style.font = "bold 18px 'Lucida Console', Monaco, monospace";
+        result.addEventListener("click", callback);
+        result.innerText = "D" + innerText;
+        return result;
+    },
+
+    increaseDetail : function() {
+        this.changeDetail(1);
+    },
+
+    decreaseDetail : function() {
+        this.changeDetail(-1);
+    },
+
+    canChangeDetail : function (delta) {
+        /* first check that we can change the detail for all layers. */
+        let canDoZoom =
+            this.internalOffsetCounter + delta <= this.maxOffset
+         && this.internalOffsetCounter + delta >= this.minOffset;
+
+        let me = this;
+
+        if (canDoZoom) {
+            this.map.eachLayer(function (layer) {
+                if ( "zoomOffset" in layer.options ) {
+                    /* when incrementing zoomOffset, we decrement
+                       maxZoom. This means that zoomOffset + maxZoom is a constant, reflecting the actual maxZoom of the map source. */
+                    let maxZoom_gaugeIndependent = layer.options.maxZoom + layer.options.zoomOffset;
+                    let minZoom_gaugeIndependent = layer.options.minZoom + layer.options.zoomOffset;
+                    let nextZoomIfApplied = layer._tileZoom + layer.options.zoomOffset + delta;
+                    canDoZoom = canDoZoom
+                        && nextZoomIfApplied >= minZoom_gaugeIndependent
+                        && nextZoomIfApplied < maxZoom_gaugeIndependent
+                          ;
+                }
+            });
+        }
+        return canDoZoom;
+    },
+
+    changeDetail : function (delta) {
+        if ( this.updateLock ) return;
+
+
+        if ( this.canChangeDetail(delta) ) {
+
+            this.internalOffsetCounter += delta;
+
+            let overallMaxZoom = -1;
+            let overallMinZoom = 1e30;
+            this.map.eachLayer(function (layer) {
+                if ( "zoomOffset" in layer.options ) {
+                    layer.options.tileSize = Math.round(Math.pow(2, -delta) * layer.options.tileSize);
+
+                    layer.options.zoomOffset += delta;
+                    layer.options.maxZoom -= delta;
+                    overallMaxZoom = Math.max(overallMaxZoom, layer.options.maxZoom);
+                    layer.options.minZoom -= delta;
+                    overallMinZoom = Math.min(overallMinZoom, layer.options.minZoom);
+
+                    layer._resetGrid();
+                }
+            });
+
+            /* need to propagate the limits to the global map */
+            this.map.setMaxZoom(overallMaxZoom);
+            this.map.setMinZoom(overallMinZoom);
+
+            /* rebuild the view */
+            this.map._resetView(this.map.getCenter(), this.map.getZoom());
+        }
+
+    }
+
+});
